e95906debb6b5923b619e991f0acd4af3b664386
StephaneBaccam <stephane.baccam@etu.univ-lyon1.fr>
2019-06-02 23:00:24 +0200

    Avancement
    3e map : NPC + Dialogue
    Test de changements de level avec un trigger

---
+++ Assets/Prefab/Scripts créée/CharacterMotor.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMotor : MonoBehaviour
+{
+    //Scripts playerinventory pour récupérer les variables 
+    PlayerInventory playerInv;
+
+    //Variables concernant les sphères à rammaser
+    public Text countSphere;
+    public int count;
+
+    //Animation
+    Animation animations;
+
+    //Vitesse déplacement
+    public float vitMarcher;
+    public float vitCourir;
+    public float vitRotation;
+
+
+    //Variables attaques
+    public float attCooldown; //Délai entre chaque attaque
+    private bool isAttacking; //Si le personnage est entrain d'attaquer ou non
+    private float currentCooldown; //Temps restant avant de réattaquer
+    public float attRange; //Distance d'attaque
+    public GameObject rayhit;
+    
+
+    //Variables sort d'attaques
+    public float coutSortFoudre;
+    public GameObject sortFoudreGO;
+    public float vitSortFoudre;
+    public GameObject rayhitsorts;
+
+    //Touches
+    public string toucheHaut;
+    public string toucheBas;
+    public string toucheRotGauche;
+    public string toucheRotDroite;
+
+    public Vector3 hauteurSaut;
+    CapsuleCollider collisionJoueur;
+
+    //Etat du perso
+    public bool isDead = false;
+
+    void Start()
+    {
+        animations = gameObject.GetComponent<Animation>();                          //la variable animations contiendra les différents animations
+        collisionJoueur = gameObject.GetComponent<CapsuleCollider>();
+        rayhit = GameObject.Find("Rayhit");
+        playerInv = gameObject.GetComponent<PlayerInventory>();
+        count = 0;
+        SetCountText();                                                             //Fonction pour afficher le texte
+    }
+
+
+    void Update()
+    {
+        if (!isDead) //Si le perso est vivant
+        {
+            //Si on ne fait rien
+            if (!Input.GetKey(toucheHaut) && !Input.GetKey(toucheBas))
+            {
+                if (!isAttacking)
+                {
+                    animations.Play("idle");
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0)) //Lorsque l'on appui sur le bouton gauche de la souris
+                {
+                    Attack(); //Appel de la fonction Attack
+                }
+                if (Input.GetKeyDown(KeyCode.R)) //Lorsque l'on appui sur le bouton droit de la souris
+                {
+                    sortAttack(); //Appel de la fonction sort d'attaque
+                }
+            }
+
+            //Marcher en avant
+            if (Input.GetKey(toucheHaut) && !Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.Translate(0, 0, vitMarcher * Time.deltaTime);
+                if (!isAttacking)
+                {
+                    animations.Play("walk");
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0)) //Lorsque l'on appui sur le bouton gauche de la souris
+                {
+                    Attack(); //Appel de la fonction Attack
+                }
+                if (Input.GetKeyDown(KeyCode.R)) //Lorsque l'on appui sur le bouton droit de la souris
+                {
+                    sortAttack(); //Appel de la fonction sort d'attaque
+                }
+            }
+
+            //Courir
+            if (Input.GetKey(toucheHaut) && Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.Translate(0, 0, vitCourir * Time.deltaTime);
+                animations.Play("run");
+            }
+
+            //Marcher en arrière
+            if (Input.GetKey(toucheBas))
+            {
+                transform.Translate(0, 0, -(vitMarcher / 2) * Time.deltaTime);
+                if (!isAttacking)
+                {
+                    animations.Play("walk");
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0)) //Lorsque l'on appui sur le bouton gauche de la souris
+                {
+                    Attack(); //Appel de la fonction Attack
+                }
+                if (Input.GetKeyDown(KeyCode.R)) //Lorsque l'on appui sur le bouton droit de la souris
+                {
+                    sortAttack(); //Appel de la fonction sort d'attaque
+                }
+            }
+
+           
+
+            //Rotation à gauche
+            if (Input.GetKey(toucheRotGauche))
+            {
+                transform.Rotate(0, -vitRotation * Time.deltaTime, 0);
+            }
+
+            //Rotation à droite
+            if (Input.GetKey(toucheRotDroite))
+            {
+                transform.Rotate(0, vitRotation * Time.deltaTime, 0);
+            }
+
+            //Sauter
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+            {
+                //Préparation du saut
+                Vector3 v = gameObject.GetComponent<Rigidbody>().velocity; //Stock la vélocité du rigidbody
+                v.y = hauteurSaut.y;
+
+                //Saut
+                gameObject.GetComponent<Rigidbody>().velocity = hauteurSaut;
+            }
+        }
+
+        if(isAttacking) //Si le personnage est en état d'attaque
+        {
+            currentCooldown -= Time.deltaTime; //réduit le délai avec le temps
+        }
+
+        if(currentCooldown<=0) //Si le délai est à 0
+        {
+            currentCooldown = attCooldown; //On remet le délai au temps de délai pour chaque attaque
+            isAttacking = false; //Le personnage n'est plus en état d'attaque
+        }
+    }
+
+    bool isGrounded()
+    {
+        return Physics.CheckCapsule(collisionJoueur.bounds.center, new Vector3(collisionJoueur.bounds.center.x, collisionJoueur.bounds.min.y - 0.1f, collisionJoueur.bounds.center.z), 0.07f, layerMask:3); //vérifie si le perso est au sol
+    }
+
+    public void Attack()  //Fonction et permet au personnage d'attaquer
+    {
+        if(!isAttacking)
+        {
+            animations.Play("attack");
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(rayhit.transform.position, transform.TransformDirection(Vector3.forward), out hit, attRange)) 
+            {
+                Debug.DrawLine(rayhit.transform.position, hit.point, Color.red); //Affiche le raycast
+                if(hit.transform.tag == "Enemy")
+                {
+                    print(hit.transform.name + " detecté ");
+                    hit.transform.GetComponent<EnemyAI>().ApplyDamage(playerInv.currentDamage);
+                }
+
+                if (hit.transform.tag == "EnemyBossLancer")
+                {
+                    print(hit.transform.name + " detecté ");
+                    hit.transform.GetComponent<EnemyAILancer>().ApplyDamage(playerInv.currentDamage);
+                }
+            }
+        }
+        isAttacking = true;
+    }
+
+    public void sortAttack()  //Fonction et permet au personnage de lancer un sort d'attaque
+    {
+        if (!isAttacking && playerInv.currentMana>=coutSortFoudre)
+        {
+            animations.Play("attack");
+            GameObject sort = Instantiate(sortFoudreGO, rayhitsorts.transform.position, transform.rotation); //Faire apparaître le sort
+            sort.GetComponent<Rigidbody>().AddForce(transform.forward * vitSortFoudre); //Envoie du sort
+            playerInv.currentMana -= coutSortFoudre; //Mana utilisée
+        }
+        isAttacking = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ObjetSpe"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            playerInv.GainExp(1000);
+        }
+    }
+
+    void SetCountText() 
+    {
+        countSphere.text = "Sphere récolté : " + count.ToString() + "/6";
+    }
+}
---
+++ Assets/Prefab/Scripts créée/CheckWeapon.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWeapon : MonoBehaviour
+{
+    //Scripts playerinventory pour récupérer les dégâts infliger par le joueur
+    PlayerInventory playerInv;
+
+    //ID de l'arme actuelle
+    private int weaponID=1;
+
+    //Liste des armes qui peuvent se trouver dans la main du personnage
+    public List<GameObject> weaponList = new List<GameObject>();
+
+    void Start()
+    {
+        playerInv = gameObject.GetComponent<PlayerInventory>();
+    }
+    void Update()
+    {
+        if(transform.childCount > 0) //Vérifie en permanance si l'objet que possède se script à un enfant (càd une arme)
+        {
+            weaponID = gameObject.GetComponentInChildren<ItemOnObject>().item.itemID; //weaponID prend l'ID de l'arme (présent dans la BDD)
+        }
+        else  //Si pas d'arme équipé
+        {
+            weaponID = 0; //0 signifie qu'il n'y a pas d'arme équipé
+            for (int i = 0; i < weaponList.Count; i++)
+            {
+                weaponList[i].SetActive(false); //On désactive l'arme qui était équipé
+            }
+        }
+
+        //A copier/coller pour chaque arme en changeant le weaponID
+
+        //Epée en fer
+        if (weaponID == 1 && transform.childCount > 0) 
+        {
+            for(int i=0; i<weaponList.Count; i++)
+            {
+                if(i == 0)
+                {
+                    weaponList[i].SetActive(true);
+                    
+                }
+            }
+        }
+        
+        //Marteau Blanc
+        if(weaponID == 2 && transform.childCount > 0)
+        {
+            for(int i = 0; i < weaponList.Count; i++)
+            {
+                if (i == 1)
+                {
+                    weaponList[i].SetActive(true);
+                    
+                }
+            }
+        }
+
+        //Hache Blanche
+        if (weaponID == 3 && transform.childCount > 0)
+        {
+            for (int i = 0; i < weaponList.Count; i++)
+            {
+                if (i == 2)
+                {
+                    weaponList[i].SetActive(true);
+
+                }
+            }
+        }
+
+    }
+}
