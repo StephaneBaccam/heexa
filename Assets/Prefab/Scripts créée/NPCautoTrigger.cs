932ee77654161a964128348afbc3836693685f5c
StephaneBaccam <stephane.baccam@etu.univ-lyon1.fr>
2019-06-03 21:47:20 +0200

    Fin quête 3
    Quête n°3 pour une des 6 sphères.
    
    Le joueur spawn dans la 3e map devant le donneur de quête.
    
    Choses à ajouter / fixes concernant le jeu en général :
    -Début du scénario (Tuto,quête 1-2)
    -Monstres à ajouter dans la map 3
    -Améliorer le système de dialogue

---
+++ Assets/Prefab/Scripts créée/NPCautoTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class NPCautoTrigger : MonoBehaviour {
+
+    public Transform ChatBackGround;
+    public Transform NPCCharacter;
+
+    private DialogueSystem dialogueSystem;
+
+    public string Name;
+
+    [TextArea(5, 10)]
+    public string[] sentences;
+
+    void Start () {
+        dialogueSystem = FindObjectOfType<DialogueSystem>();
+    }
+	
+	void Update () {
+          Vector3 Pos = Camera.main.WorldToScreenPoint(NPCCharacter.position);
+          Pos.y += 1;
+          ChatBackGround.position = Pos;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        this.gameObject.GetComponent<NPCautoTrigger>().enabled = true;
+        FindObjectOfType<DialogueSystem>().EnterRangeOfNPC();
+        if (other.gameObject.tag == "Player")
+        {
+            this.gameObject.GetComponent<NPCautoTrigger>().enabled = true;
+            dialogueSystem.Names = Name;
+            dialogueSystem.dialogueLines = sentences;
+            FindObjectOfType<DialogueSystem>().NPCName();
+        }
+    }
+
+    public void OnTriggerExit()
+    {
+        FindObjectOfType<DialogueSystem>().OutOfRange();
+        this.gameObject.GetComponent<NPCautoTrigger>().enabled = false;
+    }
+}
+
